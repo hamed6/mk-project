a6ccf0d9131dfb1a516a2f4d8409a3eb2f72a712
hamed6 <h.ghodsinezhad@gmail.com>
2022-03-31 12:20:47 +0300

    setup files

--- mclogproject/mclogproject/asgi.py
+++ mclogproject/mclogproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for macrackproject project.
+ASGI config for mclogproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macrackproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mclogproject.settings')
 
 application = get_asgi_application()
