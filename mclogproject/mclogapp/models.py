2ad43bb0f2a33e23fc6ea06339f2ccc394729941
hamed6 <h.ghodsinezhad@gmail.com>
2022-04-07 16:03:51 +0300

    model

--- mclogproject/mclogapp/models.py
+++ mclogproject/mclogapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class ShipDetails(models.Model):
-    shipImo=models.IntegerField(blank=False, null=False)
-    shipName=models.CharField(blank=False, null=False)
-    
+    shipImo=models.IntegerField(unique=True, blank=False, null=False)
+    shipName=models.CharField(max_length=25, blank=False, null=False)
+    #project number, hall number, any details about mck
     def __str__(self):
         return self.shipName
 
@@ -13,10 +13,12 @@ class ShipLogs(models.Model):
     logImo=models.ForeignKey(ShipDetails, on_delete=models.CASCADE)
     logDate=models.DateField()
     logTime=models.TimeField()
-    logCategory=models.CharField()
-    logDescription=models.CharField()
-    logExtranote=models.CharField(default='')
+    logCategory=models.CharField(max_length=10)
+    logDescription=models.CharField(max_length=100)
+    logExtranote=models.CharField(max_length=50)
     
+    class Meta:
+        ordering=['logDate']
 
     def __str__(self):
         return self.logCategory
--- mclogproject/mclogproject/settings.py
+++ mclogproject/mclogproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'maclogapp.apps.MclogappConfig'
+    'mclogapp.apps.MclogappConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'mclogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'mclog_db',
+        'USER':'mclog_db_user',
+        'PASSWORD':'mclog_db_user',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
