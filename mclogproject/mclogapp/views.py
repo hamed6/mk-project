fd318af3b56684b2232af0ab1d3c6c16db3dcbd1
hamed6 <h.ghodsinezhad@gmail.com>
2022-04-11 11:17:25 +0300

    update view

--- mclogproject/mclogapp/views.py
+++ mclogproject/mclogapp/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def index(request):
     return HttpResponse('from View')
+
+
+
+class LogFileProcess():
+    def __init__(self) -> None:
+        pass
+    
+    # get and translate file
+    def read_file(self,file):
+        imo = imo
+        self.search_ship(imo)
+        pass
+
+    def search_ship(self, imo):
+        # if yes, update related table
+        # not create a table 
+        pass
+
+
+class TransferringFile():
+     # write dynamic sql 
+    def import_file_to_db():
+        pass
+
+
+
+# presentation structure 
