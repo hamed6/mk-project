85d790e7fd311a16d014cf852651757e221073ad
hamed6 <h.ghodsinezhad@gmail.com>
2022-04-25 11:57:48 +0300

    react dom

---
+++ mclogproject/mclog_front/src/compnents/GeneralView/ShipDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+import './ShipDetails.css'
+
+
+class GeneralView extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>From Ship Details</h1>
+            </div>
+        )
+    }
+}
+
+export default GeneralView;
--- mclogproject/mclog_front/src/index.js
+++ mclogproject/mclog_front/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import  './compnents/GeneralView/ShipDetails'
 import reportWebVitals from './reportWebVitals';
+import GeneralView from './compnents/GeneralView/ShipDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GeneralView />
   </React.StrictMode>
 );
 
