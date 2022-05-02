96a8855fbbdfb424a9c4250133e4b00091693be8
hamed6 <h.ghodsinezhad@gmail.com>
2022-05-02 16:18:33 +0300

    update ui

--- mclogproject/mclog_front/src/compnents/GeneralView/ShipDetails.js
+++ mclogproject/mclog_front/src/compnents/GeneralView/ShipDetails.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { ReactDOM } from 'react'
 import './ShipDetails.css'
 import 'axios'
 import axios from 'axios'
 
-function GetShipByImo(props){
-        // GET request - ship by imo 
-        // Return data
-        axios.get('http://127.0.0.1:8000/search')
-        .then((response)=>{
-            console.log(response);
-        })
-
-}
 
 class GeneralView extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            shipResponse:[1,2,3],
+        }
+    };
+
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/search')
+        .then((response)=>
+            this.setState=({shipResponse: response.data})
+        )
+    };
+
     render(){
         return(
             <div className="container">
-                <div className="row align-items-start">
-                    <div className="col">
+            <div className="row align-items-start">
+                <div className="col">
+                    
                         <div>
                             <label>
                                 Ship IMO
@@ -41,48 +47,54 @@ class GeneralView extends React.Component{
                         <div>
                             <button>Get Ship</button>
                         </div>
-                        <div>
-                            <label>
-                                Date From:
-                            </label>
-                            <input type="date"></input>
-                            <label>To:</label>
-                            <input type="date"></input>
-                        </div>
-
-                        <div>
-                            <label>
-                                Log category
-                            </label>
-                            <select name='Log category'>
-                                <option>From db</option>
-                            </select>
-                        </div>
-
-                        <div>
+                    
+                    <div>
                         <label>
-                            Scenario query
+                            Date From:
                         </label>
-                        <select name='Scenario'>
+                        <input type="date"></input>
+                        <label>To:</label>
+                        <input type="date"></input>
+                    </div>
+
+                    <div>
+                        <label>
+                            Log category
+                        </label>
+                        <select name='Log category'>
                             <option>From db</option>
                         </select>
-                        </div>
+                    </div>
 
-                    </div>                    
-                </div>
-                <div className="row align-items-end">
-                    <div className="col">
-                        <p>
-                            Placeholder for the chart
-                        </p>
-                        <div>
-                            <GetShipByImo/>
-                        </div>
-                     </div>
-                </div>
+                    <div>
+                    <label>
+                        Scenario query
+                    </label>
+                    <select name='Scenario'>
+                        <option>From db</option>
+                    </select>
+                    </div>
+
+                </div>                    
             </div>
+            <div className="row align-items-end">
+                <div className="col">
+                    <p>
+                        Placeholder for the chart
+                    </p>
 
+                    
+                    
+                 </div>
+            </div>
             
+            <div>
+                {this.state.shipResponse.map((res)=>(
+                        <p>{res}</p>
+                    ))
+                    }
+            </div>
+        </div>
         )
     }
 }
