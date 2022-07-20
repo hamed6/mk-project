71117f4e13d40fd911b4420c8bf9ac857d3f9d16
hamed6 <h.ghodsinezhad@gmail.com>
2022-07-20 15:16:51 +0300

    update tf main

--- terraform_config/main.tf
+++ terraform_config/main.tf
@@ -6,3 +6,38 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+# Network configurations
+resource "aws_vpc" "macrack_vpc_01" {
+  cidr_block = "10.0.0.0/26"
+}
+
+resource "aws_subnet" "macrack_subnet_01" {
+  
+}
+
+resource "aws_route_table" "macrack_routetable_01" {
+  
+}
+
+resource "aws_internet_gateway" "macrack_igw_01" {
+  
+}
+
+# ----------------------------------------------------
+# Compute configurations
+resource "aws_ami" "macrack_ec2_01" {
+  
+}
+
+resource "aws_s3_bucket" "macrack_s3_01" {
+  
+}
+
+resource "aws_rds_cluster" "macrack_rds_01" {
+  
+}
